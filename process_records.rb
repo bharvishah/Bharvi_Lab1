f3dcda3ca853f97c0bbf715f7e0f23dbec804247
Jonathan Birkholz <rookieone@gmail.com>
2014-09-26 11:07:00 -0500

    moved one question to bonus

--- process_records.rb
+++ process_records.rb
@@ -6,8 +6,6 @@
 # I need you to answer a few questions for Hermes.
 
 # How much money did we make this week?
-# How much money did we make broken down by planet?
-# ie.. how much did we make shipping to Earth? Mars? Saturn? etc.
 
 # Also, bonuses are paid to employees who pilot the Planet Express
 # Different employees have favorite destinations they always pilot to
@@ -20,7 +18,11 @@
 # They get a bonus of 10% of the money for the shipment as the bonus
 # How much of a bonus did each employee get?
 
-# BONUS - uses classes for each shipment; but not necessary
+# BONUS 1
+# How much money did we make broken down by planet?
+# ie.. how much did we make shipping to Earth? Mars? Saturn? etc.
+
+# BONUS 2 - uses classes for each shipment; but not necessary
 
 open("planet_express_logs").each do |line|
   p line.chomp
